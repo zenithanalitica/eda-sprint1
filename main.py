660994ead628f729ed367416ebaa037d9aaedd69
Cyliann <maksymilian@cych.eu>
2025-05-05 15:58:24 +0200

    feat: basic querying

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ def main():
         driver.verify_connectivity()
         print(f"Auth: {driver.verify_authentication()}")
 
+    # Get the name of all 42 year-olds
+    records, summary, _ = driver.execute_query(
+        "MATCH (t:Tweet) RETURN t.id AS id LIMIT 10000",
+        database_="neo4j",
+    )
+
+    # Loop through results and do something with them
+    for tweet in records:
+        print(tweet)
+
+    # Summary information
+    print(
+        "The query `{query}` returned {records_count} records in {time} ms.".format(
+            query=summary.query,
+            records_count=len(records),
+            time=summary.result_available_after,
+        )
+    )
+
 
 if __name__ == "__main__":
     main()
